1cdc9406a206306a62af211be4d2ba18c2dd3738
Théophile YVARS <theo.yvars@gmail.com>
2021-04-26 11:16:35 +0200

    Navigation

---
+++ app/src/main/java/com/example/deviseconvert/MainFragment.java
@@ -0,0 +1,47 @@
+package com.example.deviseconvert;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainFragment extends Fragment {
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Log.i("MainFragments","Initialisation");
+
+        ImageButton buttonMenu = view.findViewById(R.id.buttonMenu);
+        ImageButton buttonInformation = view.findViewById(R.id.buttonInformation);
+
+        buttonMenu.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_mainFragment_to_menuFragment);
+        });
+
+        buttonInformation.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_mainFragment_to_informationFragment);
+        });
+
+        Log.i("MainFragments","Fin Initialisation");
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/deviseconvert/MenuFragment.java
@@ -0,0 +1,53 @@
+package com.example.deviseconvert;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MenuFragment extends Fragment {
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_menu, container, false);
+
+        Button button1 = view.findViewById(R.id.button1Menu);
+        Button button2 = view.findViewById(R.id.button2Menu);
+        Button button3 = view.findViewById(R.id.button3Menu);
+        Button button4 = view.findViewById(R.id.button4Menu);
+
+        button1.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_menuFragment_to_tauxFragment);
+        });
+
+        button2.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_menuFragment_to_convertionFragment);
+        });
+
+        button3.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_menuFragment_to_deviceFragment);
+        });
+
+        button4.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_menuFragment_to_burreauFragment);
+        });
+
+        return view;
+    }
+}
